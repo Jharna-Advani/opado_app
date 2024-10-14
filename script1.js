423d9c5d83a8f921a04b087978a4c76cf0664e3b
Jharna-Advani <jharnaadvani8@gmail.com>
2024-10-14 17:48:40 +0530

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,1 +1,10 @@
 console.log("hi");
+document.addEventListener("DOMContentLoaded", () => {
+  const footer = document.querySelector('footer');
+  if (footer) {
+    const hiddenInput = document.createElement('input');
+    hiddenInput.type = 'hidden';
+    hiddenInput.value = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.';
+    footer.appendChild(hiddenInput);
+  }
+});
